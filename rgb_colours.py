26e168d4c12f10de410e478a3bfeb537ed9d0d13
alarthast <alice.wong@thedatalab.org>
2025-05-27 17:35:34 +0100

    Implement solution with dictionary

--- rgb_colours.py
+++ rgb_colours.py
@@ -13,16 +13,18 @@
 #
 # $ python rgb_colours.py "burnt sienna"
 # I don't know the RGB code for burnt sienna
+import argparse
 
 colours = [
-    ['red', 'F00'],
-    ['yellow', 'FF0'],
-    ['green', '0F0'],
-    ['cyan', '0FF'],
-    ['blue', '00F'],
-    ['magenta', 'F0F'],
+    ["red", "F00"],
+    ["yellow", "FF0"],
+    ["green", "0F0"],
+    ["cyan", "0FF"],
+    ["blue", "00F"],
+    ["magenta", "F0F"],
 ]
 
+KNOWN_COLOURS = {i[0]: i[1] for i in colours}
 
 # TODO:
 # * Implement the program as described in the comments at the top of the file.
@@ -31,3 +33,30 @@ colours = [
 # * Change the program so that users can also enter an RGB colour code, and be
 #   told the name of the corresponding colour.
 # * Change the program so that it ignores the case of the user's input.
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="This program knows about the RGB code corresponding to common colours."
+    )
+    parser.add_argument(
+        "colour", type=str, help="Colour for which the RGB code is requested"
+    )
+    return parser
+
+
+def get_message(colour):
+    code = KNOWN_COLOURS.get(colour, None)
+    if code:
+        return f"The RGB code for {colour} is {code}"
+    return f"I don't know the RGB code for {colour}"
+
+
+def main():
+    args = get_parser().parse_args()
+    message = get_message(args.colour)
+    print(message)
+
+
+if __name__ == "__main__":
+    main()
