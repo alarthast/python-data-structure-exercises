9c4b8f4e4fa1fec07ecb9146accbb5dcea7f2d5a
Alice Wong <alice.wong@thedatalab.org>
2025-06-19 16:53:13 +0100

    Add a `main()` and a test
    To ensure refactoring doesn't break anything

--- football_results.py
+++ football_results.py
@@ -42,7 +42,6 @@ for match in RESULTS:
         HISTORY[team] = hist
 
 TOTAL_POINTS = {team: get_total_points(wdl) for team, wdl in HISTORY.items()}
-print("There were {} matches in the group".format(len(RESULTS)))
 
 
 # TODO: Write code to answer the following questions:
@@ -55,40 +54,47 @@ def get_key_for_extremum_including_ties(d: dict, func: callable):
     return ", ".join([k for k, v in d.items() if v == val])
 
 
-print(
-    "The match with the most goals was",
-    format(max(RESULTS, key=lambda match: sum(match.values()))),
-)
-print(
-    "The match with the fewest goals was",
-    format(min(RESULTS, key=lambda match: sum(match.values()))),
-)
-print(
-    "The team(s) with the most total goals was",
-    get_key_for_extremum_including_ties(TOTAL_GOALS, max),
-)
-print(
-    "The team(s) with the fewest total goals was",
-    get_key_for_extremum_including_ties(TOTAL_GOALS, min),
-)
-print(
-    "The team(s) with the most points was",
-    get_key_for_extremum_including_ties(TOTAL_POINTS, max),
-)
-print(
-    "The team(s) with the fewest points was",
-    get_key_for_extremum_including_ties(TOTAL_POINTS, min),
-)
+def main():
+    print("There were {} matches in the group".format(len(RESULTS)))
 
-# TODO (extra): Write code to compute and display a league table
-print("-" * 25)
-print(f"{'Team':<10}|G |W |D |L |P")
-print("-" * 25)
-
-for team in HISTORY.keys():
-    goals = TOTAL_GOALS[team]
-    history = HISTORY[team]
-    points = TOTAL_POINTS[team]
     print(
-        f"{team:<10}|{goals:<2}|{history.get('W',0):<2}|{history.get('D',0):<2}|{history.get('L',0):<2}|{points:<2}"
+        "The match with the most goals was",
+        format(max(RESULTS, key=lambda match: sum(match.values()))),
     )
+    print(
+        "The match with the fewest goals was",
+        format(min(RESULTS, key=lambda match: sum(match.values()))),
+    )
+    print(
+        "The team(s) with the most total goals was",
+        get_key_for_extremum_including_ties(TOTAL_GOALS, max),
+    )
+    print(
+        "The team(s) with the fewest total goals was",
+        get_key_for_extremum_including_ties(TOTAL_GOALS, min),
+    )
+    print(
+        "The team(s) with the most points was",
+        get_key_for_extremum_including_ties(TOTAL_POINTS, max),
+    )
+    print(
+        "The team(s) with the fewest points was",
+        get_key_for_extremum_including_ties(TOTAL_POINTS, min),
+    )
+
+    # TODO (extra): Write code to compute and display a league table
+    print("-" * 25)
+    print(f"{'Team':<10}|G |W |D |L |P")
+    print("-" * 25)
+
+    for team in HISTORY.keys():
+        goals = TOTAL_GOALS[team]
+        history = HISTORY[team]
+        points = TOTAL_POINTS[team]
+        print(
+            f"{team:<10}|{goals:<2}|{history.get('W',0):<2}|{history.get('D',0):<2}|{history.get('L',0):<2}|{points:<2}"
+        )
+
+
+if __name__ == "__main__":
+    main()
