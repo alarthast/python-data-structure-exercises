33f9f414318cc33ce82c9c546f1023927b8f13e1
Alice Wong <alice.wong@thedatalab.org>
2025-07-14 12:11:11 +0100

    Add the missing tests
    And add comment  to the table test

--- test_bill_splitting.py
+++ test_bill_splitting.py
@@ -10,7 +10,7 @@ def test_no_name_given():
         "Tom       |21.95",
     ]
     msg = bill_splitting.get_message(None)
-    for row in rows:
+    for row in rows:  # We don't care about the order of names
         assert row in msg
 
 
@@ -20,7 +20,20 @@ def test_no_name_given():
         (
             "Clare",
             "Clare should pay 20.9. Breakdown:\nBruschetta Originale - 5.35\nFiorentina - 10.65\nTiramasu - 4.9",
-        )
+        ),
+        (
+            "Tom",
+            "Tom should pay 21.95. Breakdown:\nCalamari - 6.0\nAmerican Hot - 11.5\nChocolate Fudge Cake - 4.45",
+        ),
+        (
+            "Rich",
+            "Rich should pay 20.9. Breakdown:\nBruschetta Originale - 5.35\nLa Reine - 10.65\nHoneycomb Cream Slice - 4.9",
+        ),
+        (
+            "Rosie",
+            "Rosie should pay 18.65. Breakdown:\nGarlic Bread - 4.35\nVeneziana - 9.4\nTiramasu - 4.9",
+        ),
+        ("Tim", "Tim did not have dinner"),
     ],
 )
 def test_person_given(person, result):
