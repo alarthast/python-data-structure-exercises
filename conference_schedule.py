88fa440e96b23d8099f74e951c50262def19b7f0
Alice Wong <alice.wong@thedatalab.org>
2025-07-22 10:32:41 +0100

    Format with ruff, add functions

--- conference_schedule.py
+++ conference_schedule.py
@@ -9,37 +9,43 @@
 # For instance:
 #
 # $ python conference_schedule.py "Main Hall" 13:30
-# 
+#
 
 
 schedule = {
-    'Main Hall': {
-        '10:00': 'Django REST framework',
-        '11:00': 'Lessons learned from PHP',
-        '12:00': "Tech interviews that don't suck",
-        '14:00': 'Taking control of your Bluetooth devices',
-        '15:00': "Fast Python? Don't Bother!",
-        '16:00': 'Test-Driven Data Analysis',
+    "Main Hall": {
+        "10:00": "Django REST framework",
+        "11:00": "Lessons learned from PHP",
+        "12:00": "Tech interviews that don't suck",
+        "14:00": "Taking control of your Bluetooth devices",
+        "15:00": "Fast Python? Don't Bother!",
+        "16:00": "Test-Driven Data Analysis",
     },
-    'Seminar Room': {
-        '10:00': 'Python in my Science Classroom',
-        '11:00': 'My journey from wxPython tp PyQt',
-        '12:00': 'Easy solutions to hard problems',
-        '14:00': 'Taking control of your Bluetooth devices',
-        '15:00': "Euler's Key to Cryptography",
-        '16:00': 'Build your Microservices with ZeroMQ',
+    "Seminar Room": {
+        "10:00": "Python in my Science Classroom",
+        "11:00": "My journey from wxPython tp PyQt",
+        "12:00": "Easy solutions to hard problems",
+        "14:00": "Taking control of your Bluetooth devices",
+        "15:00": "Euler's Key to Cryptography",
+        "16:00": "Build your Microservices with ZeroMQ",
     },
-    'Assembly Hall': {
-        '10:00': 'Distributed systems from scratch',
-        '11:00': 'Python in Medicine: ventilator data',
-        '12:00': 'Neurodiversity in Technology',
-        '14:00': 'Chat bots: What is AI?',
-        '15:00': 'Pygame Zero',
-        '16:00': 'The state of PyPy',
+    "Assembly Hall": {
+        "10:00": "Distributed systems from scratch",
+        "11:00": "Python in Medicine: ventilator data",
+        "12:00": "Neurodiversity in Technology",
+        "14:00": "Chat bots: What is AI?",
+        "15:00": "Pygame Zero",
+        "16:00": "The state of PyPy",
     },
 }
 
-print('There are talks scheduled in {} rooms'.format(len(schedule)))
+
+def get_message():
+    return "There are talks scheduled in {} rooms".format(len(schedule))
+
+
+def main():
+    return print(get_message())
 
 
 # TODO:
@@ -50,3 +56,6 @@ print('There are talks scheduled in {} rooms'.format(len(schedule)))
 #   a room at a given time, even if a session doesn't start at that time.
 # * Change the program so that if called with a single argument, the title of a
 #   session, it displays the room and the time of the session.
+
+if __name__ == "__main__":
+    main()
