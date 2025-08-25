77a54afbd21f7ff5acb0d937cb9561f93f152f94
Alice Wong <alice.wong@thedatalab.org>
2025-08-25 16:06:18 +0100

    Add test

---
+++ recursion/solutions.py
@@ -0,0 +1,6 @@
+import pathlib
+
+
+def walk(path: pathlib.Path):
+    """Return a list of all paths to files nested in path."""
+    ...
---
+++ recursion/test_walk.py
@@ -0,0 +1,29 @@
+import pathlib
+import pytest
+
+import solutions
+
+
+@pytest.mark.parametrize("walk", [solutions.walk])
+def test_walk(walk, tmpdir):
+    root_dir = pathlib.Path(tmpdir)
+    dirs = [
+        root_dir,
+        root_dir / "child_1",
+        root_dir / "child_1" / "grandchild_1",
+        root_dir / "child_2",
+    ]
+    for dir in dirs:
+        dir.mkdir(exist_ok=True)
+    files = [
+        root_dir / "root.md",
+        root_dir / "child_1" / "child_1_file_1.md",
+        root_dir / "child_2" / "child_2_file_1.md",
+        root_dir / "child_2" / "child_2_file_2.md",
+        root_dir / "child_1" / "grandchild_1" / "grandchild_1_file_1.md",
+        root_dir / "child_1" / "grandchild_1" / "grandchild_1_file_2.md",
+    ]
+    for file in files:
+        file.touch()
+
+    assert set(walk(root_dir)) == set(files)
