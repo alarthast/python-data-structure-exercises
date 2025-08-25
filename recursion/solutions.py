bae5a5881ccc507174d2a1861f494d68b02b3962
Alice Wong <alice.wong@thedatalab.org>
2025-08-25 18:10:34 +0100

    Add solution using recursion and generators
    This was the hardest. I think I would not have been able to write it
    if I didn't have `walk_1` to reference.

--- recursion/solutions.py
+++ recursion/solutions.py
@@ -8,6 +8,19 @@ def walk_1(path: pathlib.Path):  # Recusion and no generators
     return [path_ for item in path.iterdir() for path_ in walk_1(item)]
 
 
+def walk_2(path: pathlib.Path):  # Recusion and generators
+    """Return a list of all paths to files nested in path."""
+    return list(_walk_2(path))
+
+
+def _walk_2(path: pathlib.Path):
+    if not path.is_dir():
+        yield path
+    else:
+        for item in path.iterdir():
+            yield from _walk_2(item)
+
+
 def walk_3(path: pathlib.Path):  # No recursion
     """Return a list of all paths to files nested in path."""
     files = []
