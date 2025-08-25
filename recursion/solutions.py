1425d1d6a5e1bc3c104340810f7c2e8c9a9964d8
Alice Wong <alice.wong@thedatalab.org>
2025-08-25 16:10:47 +0100

    Add solution without using recursion
    I found this the easiest.
    I know how `while` loops work and I have faith that this one will get
    to every single sub-directory.
    Scanning through one directory before moving to the next is also
    something that is easy to wrap my head around.
    
    I have to keep appending to a list though. That seems like a downside
    compared to an approach that uses generators.

--- recursion/solutions.py
+++ recursion/solutions.py
@@ -1,6 +1,16 @@
 import pathlib
 
 
-def walk(path: pathlib.Path):
+def walk_3(path: pathlib.Path):  # No recursion
     """Return a list of all paths to files nested in path."""
-    ...
+    files = []
+    directories = [path]
+    while len(directories) > 0:
+        current_dir = directories.pop(0)
+        for item in current_dir.iterdir():
+            if item.is_dir():
+                directories.append(item)
+            else:
+                files.append(item)
+
+    return files
