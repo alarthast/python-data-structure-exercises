481cc7152a0483d0bcc6f58a75c3774da0e0d130
Alice Wong <alice.wong@thedatalab.org>
2025-09-08 16:14:17 +0100

    Remove tall format of data

--- browser_stats.py
+++ browser_stats.py
@@ -17,30 +17,18 @@ def get_date(year, month):
 
 
 browsers = ("chrome", "ie", "firefox", "safari", "opera")
-WideRow = collections.namedtuple("WideRow", ["date", *browsers])
-wide_data = [
-    WideRow(
+Row = collections.namedtuple("WideRow", ["date", *browsers])
+data = [
+    Row(
         date=get_date(year, month),
-        chrome=data["Chrome"],
-        ie=data["IE"],
-        firefox=data["Firefox"],
-        safari=data["Safari"],
-        opera=data["Opera"],
+        chrome=stats["Chrome"],
+        ie=stats["IE"],
+        firefox=stats["Firefox"],
+        safari=stats["Safari"],
+        opera=stats["Opera"],
     )
     for year, inner_dict in browser_stats_by_year_and_month.items()
-    for month, data in inner_dict.items()
-]
-
-TallRow = collections.namedtuple("TallRow", ["date", "browser", "market_share"])
-tall_data = [
-    TallRow(
-        date=get_date(year, month),
-        browser=browser,
-        market_share=market_share,
-    )
-    for year, inner_dict in browser_stats_by_year_and_month.items()
-    for month, data in inner_dict.items()
-    for browser, market_share in data.items()
+    for month, stats in inner_dict.items()
 ]
 
 print(
@@ -54,57 +42,62 @@ print(
 # TODO:
 # * Display a report that answers the following questions:
 #   * What period does the report cover?
-min_date = min(row.date for row in wide_data)
-max_date = max(row.date for row in wide_data)
+min_date = min(row.date for row in data)
+max_date = max(row.date for row in data)
 print(
     f"The report covers the period between {min_date.strftime('%B %Y')} and {max_date.strftime('%B %Y')}."
 )
 
+
 #   * In the period covered, which browsers have had over 50% of market share?
-browsers_with_over_half_market_share = set()
-for row in tall_data:
-    if row.market_share > 50:
-        browsers_with_over_half_market_share.add(row.browser)
+def get_browsers_with_over_half_market_share(data):
+    browsers_with_over_half_market_share = set()
+    for row in data:
+        for k, v in row._asdict().items():
+            if isinstance(v, float) and v > 50:
+                browsers_with_over_half_market_share.add(k)
+    return browsers_with_over_half_market_share
+
+
 print(
-    f'In the period covered, the following browsers had over 50% market share: {", ".join(browsers_with_over_half_market_share)}'
+    f'In the period covered, the following browsers had over 50% market share: {", ".join(get_browsers_with_over_half_market_share(data))}'
 )
 
 #   * In which month did Firefox first become the most popular browser?
-for row in sorted(wide_data, key=operator.attrgetter("date")):
+for row in sorted(data, key=operator.attrgetter("date")):
     most_popular_browser = max(browsers, key=row.__getattribute__)
     if most_popular_browser == "firefox":
         break
 print(f"Firefox first became the most popular browser in {row.date.strftime('%B %Y')}.")
 
 #   * In which month did Chrome first overtake IE in popularity?
-for row in sorted(wide_data, key=operator.attrgetter("date")):
+for row in sorted(data, key=operator.attrgetter("date")):
     if row.chrome > row.ie:
         break
 print(f"Chrome first overtook IE in popularity in {row.date.strftime('%B %Y')}.")
 
 #   * In which month was Firefox's popularity highest?
 print(
-    f"Firefox's popularity was highest in {max(wide_data, key=operator.attrgetter('firefox')).date.strftime('%B %Y')}."
+    f"Firefox's popularity was highest in {max(data, key=operator.attrgetter('firefox')).date.strftime('%B %Y')}."
 )
 
 
 #   * In which month was the combined popularity of Safari and Opera highest?
 print(
-    f"The combined popularity of Safari and Opera was highest in {max(wide_data, key=lambda row: row.safari + row.opera).date.strftime('%B %Y')}."
+    f"The combined popularity of Safari and Opera was highest in {max(data, key=lambda row: row.safari + row.opera).date.strftime('%B %Y')}."
 )
 
 
 #   * Which month saw the biggest percentage point rise in Chrome's popularity?
 ChromeRow = collections.namedtuple("ChromeRow", ["date", "current", "previous"])
-tall_data_filtered_to_chrome = sorted(
-    [row for row in tall_data if row.browser == "Chrome"],
-    key=operator.attrgetter("date"),
-)
-dates, _, current = zip(*tall_data_filtered_to_chrome[1:])
-_, _, previous = zip(*tall_data_filtered_to_chrome[:-1])
+sorted = sorted(data, key=operator.attrgetter("date"))
 chrome_data = [
     ChromeRow(date=date, current=current, previous=previous)
-    for date, current, previous in zip(dates, current, previous)
+    for date, current, previous in zip(
+        [datum.date for datum in sorted[:-1]],
+        [datum.chrome for datum in sorted[:-1]],
+        [datum.chrome for datum in sorted[1:]],
+    )
 ]
 print(
     f"The month that saw the biggest percentage point rise in Chrome's popularity was {max(chrome_data, key=lambda row: row.current - row.previous).date.strftime('%B %Y')}."
